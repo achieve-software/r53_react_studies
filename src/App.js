acb1cd17c9e7f3fea4f58bec048ffe34e45cf60a
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-21 16:10:42 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hello from './components/Hello'
+import Navbar from './components/Navbar';
 import Login from './pages/Login'
 function App() {
   return (
     <div className="App">
     <h1>react start page</h1>
+    <Navbar/>
     <Hello/>
     <Login />
     </div>
